fea09982b60f33a8f4d9929e0246341aa66460fc
Johannes Ihl <j.ihl@zema.de>
2025-04-11 10:29:06 +0200

    added vibe coding functionality, need to set up a working ollama for this

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -242,8 +242,49 @@ end,
   config = function()
     require("bufferline").setup({})
   end,
-}
+},
 
+ {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+      strategies = {
+        -- Change the default chat adapter
+        chat = {
+          adapter = 'mistral_test',
+          inline = 'mistral_test',
+        },
+      },
+      adapters = {
+        mistral_test = function()
+
+          return require('codecompanion.adapters').extend('ollama', {
+            name = 'mistral_test', -- Give this adapter a different name to differentiate it from the default ollama adapter
+            schema = {
+              model = {
+                default = 'mistral:latest',
+              },
+            },
+          })
+        end,
+      },
+      opts = {
+        log_level = 'DEBUG',
+      },
+      display = {
+        diff = {
+          enabled = true,
+          close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+          layout = 'vertical', -- vertical|horizontal split for default provider
+          opts = { 'internal', 'filler', 'closeoff', 'algorithm:patience', 'followwrap', 'linematch:120' },
+          provider = 'default', -- default|mini_diff
+        },
+      },
+    },
+  },
 
 }
 
